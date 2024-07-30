ef98874e4b7c1d7cfaed1dcf1639dc6667a72401
Mathanraj Thavamani <madhanmani0601@gmail.com>
2024-07-30 19:43:45 +0530

    Initial setup

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  Touchable,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
+  console.log("hello");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World</Text>
+      <TouchableOpacity
+        onLongPress={() => Alert.alert("hello", "wordl", [{ text: "hello" }])}
+        onPress={() => console.log(statusbar, Platform)}
+      >
+        <Image
+          source={{
+            width: 100,
+            height: 100,
+            uri: "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp",
+          }}
+        />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
