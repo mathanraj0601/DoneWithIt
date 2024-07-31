7160f53091f6b1bd5c35c8dcc123670f3a65fbea
Mathanraj <madhanmani0601@gmail.com>
2024-07-31 21:27:03 +0530

    Added Window Screen and Photo screen

--- App.js
+++ App.js
@@ -1,42 +1,6 @@
-import {
-  StyleSheet,
-  Text,
-  Touchable,
-  View,
-  Image,
-  TouchableOpacity,
-  Alert,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-} from "react-native";
+import PhotoScreen from "./app/screens/PhotoScreen";
+import WindowScreen from "./app/screens/WindowScreen";
 
 export default function App() {
-  console.log("hello");
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
-      <TouchableOpacity
-        onLongPress={() => Alert.alert("hello", "wordl", [{ text: "hello" }])}
-        onPress={() => console.log(statusbar, Platform)}
-      >
-        <Image
-          source={{
-            width: 100,
-            height: 100,
-            uri: "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp",
-          }}
-        />
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
+  return <PhotoScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ app/screens/PhotoScreen.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { StyleSheet, View, Image, SafeAreaView } from "react-native";
+import color from "../config/color";
+
+function PhotoScreen(props) {
+  return (
+    <View style={styles.phoneScreenContainer}>
+      <View style={styles.btnContainer}>
+        <View style={styles.closeBtn}></View>
+        <View style={styles.menubtn}></View>
+      </View>
+      <Image
+        style={styles.image}
+        resizeMode="contain"
+        source={require("../assets/chair.jpg")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  phoneScreenContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  btnContainer: {
+    position: "absolute",
+    top: 60,
+    width: "100%",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingHorizontal: 20,
+  },
+  closeBtn: {
+    height: 50,
+    width: 50,
+    backgroundColor: color.primary,
+  },
+  menubtn: {
+    width: 50,
+    height: 50,
+    backgroundColor: "blue",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+export default PhotoScreen;
