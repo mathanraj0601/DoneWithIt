a2c4d1117ee15081c2c685bf82cb011cbc6f5b23
Mathanraj <madhanmani0601@gmail.com>
2024-08-07 00:17:36 +0530

    Added Item Seperator component

---
+++ app/components/seperators/ListItemSeperator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+function ListItemSeperator(props) {
+  return <View style={styles.view} />;
+}
+
+const styles = StyleSheet.create({
+  view: {
+    margin: 20,
+  },
+});
+
+export default ListItemSeperator;
--- app/screens/ListScreen.js
+++ app/screens/ListScreen.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import Card from "../components/Card";
 import color from "../config/color";
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
+import ListItemSeperator from "../components/seperators/ListItemSeperator";
 
 function ListScreen() {
+  const data = [
+    {
+      id: 1,
+      name: "maddy",
+      description: "customer1",
+      image: require("../assets/splash.png"),
+    },
+    {
+      id: 2,
+      name: "yaadm",
+      description: "customer2",
+      image: require("../assets/icon.png"),
+    },
+  ];
+
   return (
     <Screen style={styles.view}>
       <ListItem
@@ -20,6 +36,19 @@ function ListScreen() {
         image={require("../assets/chair.jpg")}
         title="chair"
         subtitle="$100"
+      />
+
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <ListItem
+            image={item.image}
+            title={item.name}
+            subtitle={item.description}
+          />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        ItemSeparatorComponent={() => <ListItemSeperator />}
       />
     </Screen>
   );
